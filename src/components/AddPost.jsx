341a1dc5bd6d102e68be91a4e80818a1f916feed
dbacall <dbacall@hotmail.co.uk>
2020-07-02 16:33:13 +0100

    user can submit message

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
+import { useHistory, Redirect } from 'react-router-dom';
 
 function AddPost() {
   const [message, setMessage] = useState('');
+  let history = useHistory();
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(message);
+    setMessage('');
+    history.push('/');
+  }
 
   return (
     <div>
       <h1 id="title">Add Post</h1>
-      <form>
+      <form className="form" onSubmit={handleSubmit}>
         <input
           type="text"
           className="input"
@@ -14,9 +22,27 @@ function AddPost() {
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
+        <input type="submit" value="Post" />
       </form>
     </div>
   );
+  // redirect ? (
+  //   <Redirect to={redirect} />
+  // ) : (
+  //   <div>
+  //     <h1 id="title">Add Post</h1>
+  //     <form className="form" onSubmit={handleSubmit}>
+  //       <input
+  //         type="text"
+  //         className="input"
+  //         placeholder="Your message..."
+  //         value={message}
+  //         onChange={(e) => setMessage(e.target.value)}
+  //       />
+  //       <input type="submit" value="Post" />
+  //     </form>
+  //   </div>
+  // );
 }
 
 export default AddPost;
--- src/components/tests/AddPost.test.jsx
+++ src/components/tests/AddPost.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import AddPost from '../AddPost.jsx';
 
 describe('AddPost component:', () => {
@@ -23,4 +23,15 @@ describe('AddPost component:', () => {
     });
     expect(wrapper.find('.input').prop('value')).toBe('New message');
   });
+
+  // test('user can submit a message', () => {
+  //   wrapper = mount(<AddPost />);
+  //   wrapper.find('.input').simulate('change', {
+  //     target: { value: 'New message' },
+  //   });
+  //   wrapper.find('.form').simulate('submit', {
+  //     preventDefault: () => {},
+  //   });
+  //   expect(wrapper.find('.input').prop('value')).toBe('');
+  // });
 });
