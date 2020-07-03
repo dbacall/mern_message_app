f71740a0f28f3a1d4400216e2639759af329995a
dbacall <dbacall@hotmail.co.uk>
2020-07-03 13:38:22 +0100

    add onSubmit function for submitting a users post

--- server/app.js
+++ server/app.js
@@ -5,8 +5,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/post');
+
+var cors = require('cors');
 
 var app = express();
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 module.exports = app;
--- server/models/post.js
+++ server/models/post.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
   message: {
